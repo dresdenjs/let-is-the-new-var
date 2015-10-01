5e5571a096db822f978cb75833834401ddcc46f7
Christian Ulbrich (Zalari UG haftungsbeschränkt) <christian.ulbrich@zalari.de>
2015-10-01 17:31:57 +0200

    fix Elvis!

--- examples/fnparams_named.js
+++ examples/fnparams_named.js
@@ -1,5 +1,5 @@
 var getDocumentsForRelatedId = function({id, topic, skip = true, limit = 20}) {
-  console.log('ID: %s, topic: %s skip: %s, limit: %d', id, topic, skip ? 'true' : false, limit);
+  console.log('ID: %s, topic: %s skip: %s, limit: %d', id, topic, skip ? 'true' : 'false', limit);
 };
 
 getDocumentsForRelatedId({id:100, topic:'JavaScript', skip:false, limit:100});
--- examples/fnparams_optional.js
+++ examples/fnparams_optional.js
@@ -1,7 +1,7 @@
 var getDocumentsForRelatedId = function(id, topic, skip, limit) {
   skip = skip || true;
   limit = limit || 20;
-  console.log('ID: %s, topic: %s skip: %s, limit: %d', id, topic, skip ? 'true' : false, limit);
+  console.log('ID: %s, topic: %s skip: %s, limit: %d', id, topic, skip ? 'true' : 'false', limit);
 };
 
 getDocumentsForRelatedId(100, 'JavaScript', false, 100);
